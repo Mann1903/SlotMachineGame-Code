8b6dc1c093c63d0cc16982392e3b7109e9f6d2b7
Mann1903 <manndsylva@gmail.com>
2024-07-21 11:54:06 +0530

    first commit

---
+++ MauiApp4/Enums/SlotSymbols.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slotApp_txt.Enums
+{
+    public enum SlotSymbols
+    {
+        num0=0,
+        num1=1,
+        num2=2,
+        num3=3,
+        num4=4,
+        num5=5,
+        num6=6,
+        num7=7,
+        num8=8,
+        num9=9
+
+    }
+    /*public class Constants
+    {
+        public static readonly string[] symobls = 
+        {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"
+        };
+    }*/
+}
